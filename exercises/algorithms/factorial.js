28247f7cd7e61f41ea24804412e143c5190759fe
briptimus <brituor@gmail.com>
2015-05-25 13:10:27 -0700

    implemented recursive factorial algorithm

--- exercises/algorithms/factorial.js
+++ exercises/algorithms/factorial.js
@@ -1,6 +1,12 @@
 //Write a factorial function in an iterative and recursive way.
 function recursiveFactorial(n){
+	if (n < 0)
+		throw new Error("input is a negative number");	
 
+	if(n <= 1)
+		return 1;
+
+	return n * recursiveFactorial(n - 1);
 }
 
 function iterativeFactorial(n){
