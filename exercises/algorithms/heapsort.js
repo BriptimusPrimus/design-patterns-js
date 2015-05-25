e4577905aae2fb50a48a227a24e6efa9bf5f1d09
briptimus <brituor@gmail.com>
2015-05-25 16:27:23 -0700

    wrote tests for heapsort algorithm

---
+++ exercises/algorithms/heapsort.js
@@ -0,0 +1,12 @@
+//http://en.wikipedia.org/wiki/Heapsort
+//The complete binary tree maps the binary tree structure into 
+//the array indices; each array index represents a node; 
+//For a zero-based array, the root node is stored at index 0; 
+//if i is the index of the current node, then
+//iParent     = floor((i-1) / 2)
+//iLeftChild  = 2*i + 1
+//iRightChild = 2*i + 2
+
+function heapsort(A){
+
+}
