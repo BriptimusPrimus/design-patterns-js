0e869509fd65157317c91048b255a4546dd61691
briptimus <brituor@gmail.com>
2015-05-25 17:10:36 -0700

    wrote tests for quicksort algorithm

---
+++ exercises/algorithms/quicksort.js
@@ -0,0 +1,8 @@
+//http://en.wikipedia.org/wiki/Quicksort
+//a very commonly used algorithm for sorting. When implemented well, 
+//it can be about two or three times faster than its main competitors, 
+//merge sort and heapsort.
+
+function quicksort(A){
+
+}
